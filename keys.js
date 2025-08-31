e5d66539f6d52f21b55ea977b47654b6295684da
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-01 00:31:46 +0600

    for of array

---
+++ keys.js
@@ -0,0 +1,14 @@
+const glass = {
+    name: 'glass',
+    color: 'golden',
+    price: 12,
+    isCleaned: true
+}
+
+console.log(glass)
+
+const keys =Object.keys(glass);
+const values =Object.values(glass);
+const entries =Object.entries(glass);
+
+console.log(entries)
---
+++ looping.js
@@ -0,0 +1,10 @@
+const numbers =[30,63,96,54,12,15,2,3];
+
+for(const num of numbers){
+    // console.log(num)
+}
+
+const name ='Tanzid Anan';
+for(const char of name){
+    console.log(char)
+}
