32ad121c8ab968bdd28803f580028eb4985fda4d
TanzidAnan <tanzid.anan125@gmail.com>
2025-08-31 23:32:16 +0600

    max number

--- spreact.js
+++ spreact.js
@@ -1,2 +1,8 @@
 const max =Math.max(6,96,36,25,89,74,14,95,14);
-console.log(max)
+// console.log(max)
+
+
+const numbers =[32,87,45,68,41,56,15,63];
+const arrayMax =Math.max(numbers);
+console.log(...numbers)
+console.log(arrayMax)
