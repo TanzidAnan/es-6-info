88412bb94fb84767202c2f2ea12a3b3e499505a1
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-01 00:04:39 +0600

    destrating

---
+++ destructuring.js
@@ -0,0 +1,12 @@
+const actor ={
+    name:'Ananata',
+    age:30,
+    phone:'019121212',
+    money:121212121
+}
+
+const {name} =actor
+
+const phone =actor.phone;
+// console.log(phone)
+console.log(name)
--- spreact.js
+++ spreact.js
@@ -3,6 +3,16 @@ const max =Math.max(6,96,36,25,89,74,14,95,14);
 
 
 const numbers =[32,87,45,68,41,56,15,63];
-const arrayMax =Math.max(numbers);
-console.log(...numbers)
-console.log(arrayMax)
+const arrayMax =Math.max(...numbers);
+// console.log(...numbers)
+// console.log(arrayMax)
+
+const friends =[656,65,65,9]
+const dosto = [... friends] // copy
+console. log (dosto)
+friends. push (100)
+console. log (dosto)
+console. log (friends)
+// advanced
+const sonkha = [...friends, 9999] // add extra elements while copy
+console. log (sonkha)
