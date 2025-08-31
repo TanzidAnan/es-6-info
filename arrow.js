eb19b8d66478b7c4047efcca5919b238729ea307
TanzidAnan <tanzid.anan125@gmail.com>
2025-08-31 21:11:55 +0600

    arrow funcation

--- arrow.js
+++ arrow.js
@@ -5,9 +5,27 @@
 // const sum =add(5,20);
 // console.log(sum)
 
-
+// function declatation
 function  add (a,b){
     return a+b
 }
 const sum =add(5,20);
-console.log(sum)
+// console.log(sum);
+
+
+// function expression
+
+const add2 = function(a,b){
+    return a+b;
+}
+
+const sum2 =add2(2,3)
+console.log(sum2)
+
+// arrow function
+const add3 =(a,b) =>{
+    return a+b;
+}
+
+const sum3 =add3(25,30);
+console.log(sum3)
