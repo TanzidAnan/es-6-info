780360ac424d6dc716961b6fbd02c08bde5690c3
TanzidAnan <tanzid.anan125@gmail.com>
2025-08-31 20:44:06 +0600

    \m ata kora jai

--- template.js
+++ template.js
@@ -1,6 +1,19 @@
 const fast ='Tanzid';
 const last ='Anan';
 
-const name =fast+ last;
+const name =fast + ' '+ last;
 
-console.log(name)
+// console.log(name)
+
+const a= 50;
+const b =40
+
+
+const math =`The sum of ${a} and ${b} is ${a+b}`;
+console.log(math);
+
+const email ='hi john \n'+
+'cena cena lage \n'+
+'valo lage ';
+
+console.log(email)
