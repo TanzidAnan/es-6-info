cf7b8a4f6ad85b8c409e4109095856c969c32413
TanzidAnan <tanzid.anan125@gmail.com>
2025-08-31 21:22:38 +0600

    single parameter or

---
+++ more-arrow.js
@@ -0,0 +1,15 @@
+const difference = (x, y) => x - y;
+const multipul = (first, second, third) => first * second * third;
+
+const getAge =(person) => {
+    return person.age
+};
+const student = {name:'tanzid',age:50};
+const age =getAge(student);
+// console.log(age)
+
+
+const getThird =numbers => numbers[2];
+
+const third =getThird([12,36,35,26,32,45,36]);
+console.log(third)
