59891f8b0f432bd0abde386e3c1a9aa775c00beb
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-06 01:22:38 +0600

    settime out

---
+++ more-js-concepts/interval.js
@@ -0,0 +1,12 @@
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+setTimeout(() =>{
+    console.log(212)
+},3000)
+console.log(5)
+console.log(6)
+console.log(7)
+console.log(8)
+console.log(9)
