61398eb5bc3796b78c573850da0fed3920638d48
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-05 23:49:07 +0600

    funcation

---
+++ more-js-concepts/async.js
@@ -0,0 +1,10 @@
+console.log(2)
+console.log(2)
+doSomething()
+console.log(2)
+console.log(2)
+
+
+function doSomething(){
+    console.log(3)
+}
---
+++ more-js-concepts/context.js
@@ -0,0 +1,10 @@
+let a =5;
+
+function add(num1,num2){
+    const result =num1+num2 +a;
+    return result
+}
+
+
+const sum =add(2,5);
+console.log(sum)
