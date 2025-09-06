e4dea947f2856686d4ab7bae6e5539bc5728c17d
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-06 23:50:37 +0600

    try charch

--- more-js-concepts/try-cach.js
+++ more-js-concepts/try-cach.js
@@ -1,10 +1,15 @@
 function checkAge(){
     const ageFied =document.getElementById('age').value;
+    const errorTag =document.getElementById('error')
     try{
-
+        console.log(tanzid )
     }
     catch(error){
         console.log("Error",error)
+        errorTag.innerText=`something wrong`
+    }
+    finally{
+        console.log('all done inside try catch')
     }
 }
 
